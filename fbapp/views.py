caa010409f1738622ccbb16e7cb68ffd0554f5d8
Abdalodj <lodianea98@gmail.com>
2020-07-07 22:57:09 +0000

    Part2, Chapter 1

--- fbapp/views.py
+++ fbapp/views.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html')
+    description = """
+        Toi, tu n'as pas peur d'être seul ! Les grands espaces et les 
+        aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton 
+        émission préférée ! Bientôt tu partiras les cheveux au vent 
+        sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle 
+        chance ! 
+    """
+    return render_template('index.html', 
+                            user_name="Tom",
+                            user_image=url_for('static', filename='img/profile.png'),
+                            description = description,
+                            blur=True)
+
+
+@app.route('/result/')
+def result():
+    description = """
+        Toi, tu n'as pas peur d'être seul ! Les grands espaces et les 
+        aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton 
+        émission préférée ! Bientôt tu partiras les cheveux au vent 
+        sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle 
+        chance ! 
+    """
+    return render_template('result.html', 
+                            user_name="Tom",
+                            user_image=url_for('static', 
+                            filename='tmp/cover_111823112767411.jpg'),
+                            description = description)
 
 # if __name__ == "__main__":
 #     app.run()
