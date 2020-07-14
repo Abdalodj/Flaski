ecceead9ca92348bf6e0fa3bf3ab9501cf210b4c
Abdalodj <lodianea98@gmail.com>
2020-07-14 22:14:26 +0000

    Part 3, Chapter 2

--- fbapp/utils.py
+++ fbapp/utils.py
@@ -1,7 +1,66 @@
 import random
+import os
+import textwrap
+
+from PIL import Image, ImageFont, ImageDraw
 
 from .models import Content, Gender
 
 def find_content(gender):
     content = Content.query.filter(Content.gender == Gender[gender]).all()
     return random.choice(content)
+
+class OpenGraphImage:
+    def __init__(self, uid, firstname, description):
+        self.location = self._location(uid)
+        background = self.base()
+        self.print_on_img(background, firstname.capitalize(), 70, 50)
+
+        # textwrap découpe une chaine de caractères
+        # sans couper les mots au milieu.
+        sentences = textwrap.wrap(description, width=60)
+
+        # current_h : Hauteur à la laquelle commence à écrire.
+        # pad : pixels à ajouter entre chaque ligne, en hauteur.
+        current_h, pad = 180, 10
+
+        for sentence in sentences:
+            w, h = self.print_on_img(background, sentence, 40, current_h)
+
+            # on incrémente la hauteur pour créer une nouvelle ligne
+            # en-dessous.
+            current_h += h + pad
+        
+        background.save(self._path(uid))
+    
+    def _location(self, uid):
+        return 'tmp/{}'.format(uid)
+
+    def base(self):
+        # create a basic image
+        img = Image.new('RGB', (1200,630), "#18BC9C")
+        return img
+    
+    def _path(self, uid):
+        # Le nom de l'image est l'identifiant Facebook de la personne
+        # Cela nous garantit de ne pas avoir de doublon.
+        return os.path.join('fbapp', 'static', 'tmp', '{}.jpg'.format(uid))
+    
+    def print_on_img(self, img, text, size, height):
+        # On commence par charger la police utilisée.
+        font = ImageFont.truetype(os.path.join('fbapp', 'static', 'fonts', 'Arcon-Regular.otf'), size)
+
+        # Création d'une nouvelle instance
+        draw = ImageDraw.Draw(img)
+
+        # textsize renvoie la largeur et la hauteur en pixel
+        # d'une chaine de caractères donnée.
+        w, h = draw.textsize(text, font)
+
+        # Calcul de la position pour que le texte soit centré
+        # et non pas aligné à gauche.  
+        position = ((img.width - w) / 2, height)
+
+        # Ajout du texte à l'image.
+        draw.text(position, text, (255, 255, 255), font=font)
+        return w, h
--- fbapp/views.py
+++ fbapp/views.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config.from_object('config')
 # To get one variable, tape app.config['MY_VARIABLE']
 
-from .utils  import find_content
+from .utils  import find_content, OpenGraphImage
 
 @app.route('/')
 @app.route('/index/')
@@ -16,15 +16,16 @@ def index():
         og_image = url_for('static', filename=img, _external=True)
         og_url = url_for('index', img=img, _external=True)
     else:
-        description = """
+        og_image = url_for('static', filename='tmp/sample.jpg', _external=True)
+        og_url = url_for('index', _external=True)
+    
+    description = """
             Toi, tu n'as pas peur d'être seul ! Les grands espaces et les 
             aventures sont faits pour toi. D'ailleurs, Koh Lanta est ton 
             émission préférée ! Bientôt tu partiras les cheveux au vent 
             sur ton radeau. Tu es aussi un idéaliste chevronné. Quelle 
             chance ! 
         """
-        og_image = url_for('static', filename='tmp/sample.jpg', _external=True)
-        og_url = url_for('index', _external=True)
 
     return render_template('index.html', 
                             user_name="Tom",
@@ -42,7 +43,7 @@ def result():
     user_name = request.args.get('first_name')
     uid = request.args.get('id')
     profil_pic = 'http://graph.facebook.com/' + uid + '/picture?type=large'
-    img = 'tmp/sample.jpg'
+    img = OpenGraphImage(uid, user_name, description).location
     og_url = url_for('index', _external=True,img=img)
     return render_template('result.html', 
                             user_name=user_name,
